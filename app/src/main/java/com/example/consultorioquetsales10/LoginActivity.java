f6d5f369b79eb56f43bf6246eb27455ef8b738cb
cinth <cinthyaprieto720@gmail.com>
2024-11-02 19:10:55 -0600

    Actulizaciones con dialogos de login(acceso correcto o incorrecto)

--- app/src/main/java/com/example/consultorioquetsales10/LoginActivity.java
+++ app/src/main/java/com/example/consultorioquetsales10/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.consultorioquetsales10;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,11 +31,9 @@ public class LoginActivity extends AppCompatActivity {
 
                 }else{
                     if(login_username.getText().toString().trim().equals("Doctor") && login_password.getText().toString().trim().equals("12345")){
-                        Toast.makeText(LoginActivity.this,"Bienvenido!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
-                        startActivity(intent);
+                        showSuccessDialog();
                     }else {
-                        Toast.makeText(LoginActivity.this,"Acceso Incorrecto!", Toast.LENGTH_SHORT).show();
+                        showDeniedDialog();
                     }
                 }
             }
@@ -56,5 +58,51 @@ public class LoginActivity extends AppCompatActivity {
             login_password.setError(null);
             return true;
         }
+    }
+
+    private void showSuccessDialog(){
+        ConstraintLayout successConstraintLayout = findViewById(R.id.successConstraintLayout);
+        View view = LayoutInflater.from(LoginActivity.this).inflate(R.layout.success_dialog, successConstraintLayout);
+        Button successDone = view.findViewById(R.id.successDone);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        successDone.findViewById(R.id.successDone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+        if (alertDialog.getWindow()!=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
+    }
+
+    private void showDeniedDialog(){
+        ConstraintLayout deniedConstraintLayout = findViewById(R.id.deniedConstraintLayout);
+        View view = LayoutInflater.from(LoginActivity.this).inflate(R.layout.login_denied_dialog, deniedConstraintLayout);
+        Button deniedDone = view.findViewById(R.id.deniedDone);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        deniedDone.findViewById(R.id.deniedDone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+                Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        if (alertDialog.getWindow()!=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
     }
 }
